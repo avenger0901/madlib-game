1c347b9b5a43ce14624c8271d46472bf6a341387
zhao ziyi <zhaoziy@oregonstate.edu>
2020-01-12 13:27:24 -0800

    adding new changes

---
+++ madlib.js
@@ -0,0 +1,7 @@
+function fillInBlanks(){
+    
+    const adj1Input = document.getElementById('adj1Input');
+    const adj1Output = adj1Input.value;
+    adj1.textContent = adj1Output;
+    document.getElementById("storyresults").style.visibility = "visible";
+}
---
+++ testing.js
@@ -0,0 +1,47 @@
+function promptMe(){
+    var adj = prompt("Please provide an Adjective");
+    adjinput.textContent=adj;
+    var noun=prompt("Please give me an noun");
+    nouninput.textContent=noun;
+   var verb1=prompt("Please give me a verb in past tense:")
+   verbpast.textContent=verb1;
+    var adverb=prompt("Please give ma an adverb:")
+    adverbinput.textContent=adverb;
+    var adj2=prompt("Please give me another adjective:");
+    adj2input.textContent=adj2;
+    var noun2=prompt("Please give me another noun:");
+    noun2input.textContent=noun2;
+    var noun3=prompt("Anothe noun:");
+    noun3input.textContent=noun3;
+    var adj3=prompt("Another adjective:");
+    adj3input.textContent=adj3;
+    var verb2=prompt("Another verb:");
+    verb1input.textContent=verb2;
+    var adb2=prompt("Type another adverb:");
+    adverb2.textContent=adb2;
+    var vbpast2=prompt("Give me another verb in past tense:")
+    verbpast2.textContent=vbpast2;
+    var lstadj=prompt("Almost done! Give me last adjective, Pleeeeease");
+    lastadjective.textContent=lstadj;
+    
+
+
+
+
+
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+    storyResults.style.display="block";
+}
